5553a79ad01a8e9e746cee1c1934dbe8dbc45422
nicolasdca <nicolasdavid193@gmail.com>
2023-04-27 11:40:23 -0300

    Aula 2.1

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1;
+const maiorValor = 123;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * maiorValor + 1);
+}
+
+// document.querySelector('#menor-valor').innerHTML = menorValor;
+// document.querySelector('#maior-valor').innerHTML = maiorValor;
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
